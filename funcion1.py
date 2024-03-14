f049622d1bd1e534b66a5ac5b61121b73d29ffa2
Usaex11 <157865999+Usaex11@users.noreply.github.com>
2024-03-13 18:51:24 -0600

    Add files via upload
    si logro recrear la funcion que me pedian no se porque solo reinicie mi laptop segun yo no hice muchos cambios significativos

--- funcion1.py
+++ funcion1.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD, RMSprop, Adam
+from tensorflow.keras.layers import Input
 from matplotlib import pyplot as plt
 import numpy as np
 import math
@@ -11,35 +12,42 @@ import math
 #f = 3 * np.sin(np.pi * x)
 def f(x):
     return 3 * np.sin(np.pi * x)
+learning_rate=0.0001
+optimizer='Adam'
+epocas=3500
+batch=10000
 
 
 
 model = Sequential()
-model.add(Dense(200, activation='tanh',  input_shape=(1,)))
+model.add(Input(shape=(1,)))
+model.add(Dense(512, activation='tanh'))
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(64, activation='tanh'))
+model.add(Dense(200, activation='tanh'))
 model.add(Dense(1))
-model.compile(optimizer=Adam(learning_rate=0.0001), metrics=['loss'])
+model.compile(optimizer=Adam(learning_rate=learning_rate), metrics=['loss'])
 model.summary()
 
-x_train = np.random.uniform(-10, 10, size=(100, 1))
+x_train = np.random.uniform(-10, 10, size=(batch, 1))
 y_train = f(x_train)
 x_test = np.linspace(-10, 10, 100).reshape(-1, 1)
 y_test = f(x_test)
 
 
-model.compile(loss='mean_squared_error',optimizer='Adam')
+model.compile(loss='mean_squared_error',optimizer=optimizer)
 history = model.fit(x_train, y_train,
-                    batch_size=100,
-                    epochs=10000,
-                    verbose=1,
+                    batch_size=batch,
+                    epochs=epocas,
+                    verbose=3,
                     validation_data=(x_test, y_test))
 
 a = model.predict(x_test)
 
 
-plt.plot(history.history["loss"])
+plt.plot(history.history["loss"], label='Funcion de costo')
+plt.legend()
+
 plt.figure(figsize=(8, 6))
 plt.plot(x_test, f(x_test), label='Función Objetivo', color='blue')
 plt.plot(x_test, a, label='Predicciones', color='red', linestyle='--')
